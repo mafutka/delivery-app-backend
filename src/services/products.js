ad4caa31b761bc77292f6f983707d48d3c096822
mafutka <mafutka@gmail.com>
2026-03-26 11:04:24 +0200

    backend-completed

--- src/services/products.js
+++ src/services/products.js
@@ -1,44 +1,46 @@
 import Product from "../models/products.js"
 
-export const getAllProducts = async () => {
-  try {
-    // Повертаємо всі продукти без фільтрів
-    const products = await Product.find().populate("shopId");
-    return products;
-  } catch (error) {
-    console.log("Error fetching products:", error);
-    throw error;
+// export const getAllProducts = async () => {
+//   try {
+//     // Повертаємо всі продукти без фільтрів
+//     const products = await Product.find().populate("shopId");
+//     return products;
+//   } catch (error) {
+//     console.log("Error fetching products:", error);
+//     throw error;
+//   }
+// };
+
+export const getAllProducts = async ({ category, sort, page = 1, limit = 10 }) => {
+  let query = {}
+
+  if (category) {
+    query.category = category
+    console.log(category)
   }
-};
 
-// export const getAllProducts = async ({ category, sort, page = 1, limit = 10 }) => {
-//   let query = {}
+  let mongoQuery = Product.find(query).populate("shopId")
 
-//   if (category) {
-//     query.category = category
-//     console.log(category)
-//   }
+  if (sort === "price_asc") {
+    mongoQuery = mongoQuery.sort({ price: 1 })
+  }
 
-//   let mongoQuery = Product.find(query).populate("shopId")
-//   console.log(products)
+  if (sort === "price_desc") {
+    mongoQuery = mongoQuery.sort({ price: -1 })
+  }
 
-//   if (sort === "price_asc") {
-//     mongoQuery = mongoQuery.sort({ price: 1 })
-//   }
+  if (sort === "name") {
+    mongoQuery = mongoQuery.sort({ name: 1 })
+  }
 
-//   if (sort === "price_desc") {
-//     mongoQuery = mongoQuery.sort({ price: -1 })
-//   }
+  const pageNumber = Number(page) || 1
+const limitNumber = Number(limit) || 10
 
-//   if (sort === "name") {
-//     mongoQuery = mongoQuery.sort({ name: 1 })
-//   }
+const skip = (pageNumber - 1) * limitNumber
 
-//   const skip = (page - 1) * limit
+  mongoQuery = mongoQuery.skip(skip).limit(Number(limit))
 
-//   mongoQuery = mongoQuery.skip(skip).limit(Number(limit))
+  const products = await mongoQuery
 
-//   const products = await mongoQuery
-
-//   return products
-// }
+  return products
+}
